f8b0bb41c97be85fa71d2bad8ffa680e1f1894ec
Jordan <jordanboazr@gmail.com>
2021-04-10 23:58:16 -0300

    feature/transactionaltable

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 
 import { Sumary } from '../Summary'
+import { TransactionsTable } from '../TransactionsTable'
 import { Container } from './style'
 
 export function Dashboard() {
   return (
     <Container>
       <Sumary />
+      <TransactionsTable />
     </Container>
   )
 }
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,41 @@
+import { Container } from './styles'
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Value</th>
+            <th>Category</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Software developing</td>
+            <td className="deposit">$7.000,00</td>
+            <td>Programming</td>
+            <td>20/12/2021</td>
+          </tr>
+
+          <tr>
+            <td>Rent</td>
+            <td className="withdraw">-$7.000,00</td>
+            <td>Programming</td>
+            <td>20/12/2021</td>
+          </tr>
+
+          <tr>
+            <td>Software developing</td>
+            <td>$7.000,00</td>
+            <td>Programming</td>
+            <td>20/12/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
