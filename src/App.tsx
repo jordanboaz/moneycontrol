8acb314ca85fcfe33be7e59299dacf029e5ce20f
Jordan <jordanboazr@gmail.com>
2021-04-12 11:06:42 -0300

    Adds modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import Modal from 'react-modal'
+import { useState } from 'react';
 
+Modal.setAppElement('#root')
 
 export function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function handleOpenModal() {
+    setModalIsOpen(true)
+  }
+
+  function handleCloseModal() {
+    setModalIsOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenModal} />
       <Dashboard />
+
+      <Modal isOpen={modalIsOpen} onRequestClose={handleCloseModal}>
+        <h2>Register new transaction</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
